ecc41a282eee6ac5f8f7cb27d164a962def7924b
lieman27 <lieman27@bergen.org>
2024-01-09 15:13:53 -0500

    sprite screen

--- main.gd
+++ main.gd
@@ -3,18 +3,4 @@ extends Node2D
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
-	pass # Replace with function body.
-
-
-# Called every frame. 'delta' is the elapsed time since the previous frame.
-func _process(delta):
-	pass
-
-
-func _on_button_pressed():
-	get_tree().quit() # Replace with function body.
-
-
-
-func _on_button_2_pressed():
-	get_tree().quit() # Replace with function body.
+	$AnimationPlayer.play("right")
